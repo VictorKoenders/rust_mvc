6fb4dd7b206276f496c691df25b0c07c4dea07c6
Victor Koenders <victor.koenders@gmail.com>
2018-04-06 23:12:18 +0200

    Emptied crate

--- src/build/error.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug)]
-#[allow(dead_code)]
-pub enum ParseError {
-	FileError(String),
-	UnexpectedNode(String)
-}
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(rustc_private)]
-extern crate syntax;
-extern crate hyper;
-
-pub mod build;
-pub mod view;
-pub mod server;
